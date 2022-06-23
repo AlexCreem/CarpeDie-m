85a95a6d71b73b70a32b296bc711c444515304e5
Creem <creema@wit.edu>
2022-06-22 22:03:06 -0400

    better shooting

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,24 +4,44 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float speed = 20f;
+    public float speed = 10f;
     public Rigidbody2D rb;
     public int damage = 20;
     void Start()
     {
-        rb.velocity = transform.right * speed;
+        Vector3 shootDirection = Input.mousePosition;
+        shootDirection.z = 0.0f;
+        shootDirection = Camera.main.ScreenToWorldPoint(shootDirection);
+        shootDirection = shootDirection - transform.position;
+        rb.velocity = shootDirection.normalized * speed;
+        //rb.velocity = transform.right * speed;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log(collision.name);
+        if (!collision.name.Equals("Player"))
+        {
+            Enemy enemy = collision.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+                Destroy(this.gameObject);
+                Debug.Log("got here");
+            }
+
+            Debug.Log(collision.name);
+        }
+        /*
         Enemy enemy = collision.GetComponent<Enemy>();
         if (enemy != null)
         {
             enemy.TakeDamage(damage);
+            Destroy(this.gameObject);
         }
 
         Debug.Log(collision.name);
-
+        */
        //Destroy(this.gameObject);
     }
 }
